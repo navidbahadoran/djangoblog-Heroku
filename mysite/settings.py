07090fe40d8b31e61384579f6904001f76b48ec5
Navid Bahadoran <navid.bahadoran@gmail.com>
2019-08-19 20:49:44 -0700

    setting update

--- mysite/settings.py
+++ mysite/settings.py
@@ -183,4 +183,4 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
+# del DATABASES['default']['OPTIONS']['sslmode']
